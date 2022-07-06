b530fdfdd6a155643d933fb6f53a4da7e2c83cb9
ituarte00 <83104435+ituarte00@users.noreply.github.com>
2022-07-06 18:33:24 -0300

    Add files via upload

---
+++ Sprint2/SistemaDeTurnos/src/Paciente.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class Paciente extends Persona{
+    private String obraSocial;
+    private int nroAfiliado;
+    private ArrayList<Turno> turnos;
+
+
+    public Paciente(String nombre, String apellido, String direccion, long telefono, int dni, String email, String obraSocial, int nroAfiliado) {
+        super(nombre, apellido, direccion, telefono, dni, email);
+        this.turnos=new ArrayList<Turno>();
+        this.obraSocial=obraSocial;
+        this.nroAfiliado = nroAfiliado;
+    }
+
+    public int getAfiliado () {
+        return this.nroAfiliado;
+    }
+
+    public boolean cancelarTurno(Turno t){
+        return turnos.remove(t);
+    }
+
+    public ArrayList<Turno> verTurnos() {
+        return new ArrayList<Turno> (this.turnos);
+    }
+
+    public String getObraSocial() {
+        return obraSocial;
+    }
+
+    public boolean addTurno(Turno turno){
+        return(turnos.add(turno));
+    }
+}
